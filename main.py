e0a05264cfb617117d505ef8e3809aa4b6711a68
IvaN13BataeV <forward1301@mail.ru>
2024-01-31 23:35:44 +0500

    changed main.py

--- main.py
+++ main.py
@@ -1346,7 +1346,148 @@
 # a.clear()
 # print(a)
 
-print("Данные для добавления на GitHub")
+# print("Данные для добавления на GitHub")
+
+# s = ['ab_1', 'ac_2', 'bc_1', 'bc_2']
+# # a = [i for i in s if 'a' not in i]
+# # a = ['A' + i[1:] if i[0] == 'a' else 'B' + i[1:] for i in s]
+# a = ['A' + i[1:] if i[0] == 'a' else 'B' + i[1:] for i in s if i[1] == 'c']
+# print(a)
+# print(['A' + i[1:] if i[0] == 'a' else 'B' + i[1:] for i in ['ab_1', 'ac_2', 'bc_1', 'bc_2'] if i[1] == 'c'])
+# тернарное выражение q = True if условие else False
+# lst = []
+# for i in s:
+#     if i[1] == 'c':
+#         if i[0] == 'a':
+#             lst.append('A' + i[1:])
+#         else:
+#             lst.append('B' + i[1:])
+# print(lst)
+
+# a = {0, 1, 2, 3}
+# b = {4, 3, 2, 1}
+# c = a | b  # объединение множеств в другое
+# c = a & b  # пересечение множеств в другом
+# c = a - b  # разница множеств в другом
+# c = a ^ b
+# c = a.union(b)
+# print(c)
+# a |= b  # добавление в текущее множество
+# a &= b
+# a -= b
+# a ^= b
+# print(a)
+
+# s1 = {1, 2}
+# s2 = {3}
+# s3 = {4, 5}
+# s4 = {3, 2, 6}
+# s5 = {6}
+# s6 = {7, 8}
+# s7 = {9, 8}
+# # s = s1.union(s2, s3, s4, s5, s6, s7)
+# s = s1 | s2 | s3 | s4 | s5 | s6 | s7
+# print(s)
+# print(len(s))
+# print(min(s))
+# print(max(s))
+#
+# s1 = "Hello"
+# s2 = "How are you"
+# s = set(s1) & set(s2)
+# print(s)
+# # for i in s:
+# #     print(i, end=" ")
+
+# a = "Python"
+# b = "Programming language"
+# print(set(a) - set(b))
+
+# drawing = {'Марина', 'Женя', 'Света'}
+# music = {'Костя', 'Женя', 'Илья'}
+# one_hobby = drawing ^ music
+# print(one_hobby)
+# both_hobby = drawing & music
+# print(both_hobby)
+# drawing -= both_hobby
+# print(drawing)
 
 
+# a = {0, 1, 2, 3, 4}
+# b = {3, 2, 1}
+# print(a < b)
 
+# frozenset - неизменяемое множество
+
+# s = frozenset([1, 2, 3, 4, 5, 6])
+# s = frozenset("hello")
+# print(s)
+
+
+# Словари (dict)
+
+# s = [1, 2, 3]
+# d = {"one": 1, "two": 2, "three": 3}
+# print(s[1])
+# print(d["two"])
+#
+# s1 = ["one", "two", "three"]
+# d1 = {1: "one", 2: "two", 3: "three"}
+# print(s1[1])
+# print(d1[2])
+
+# d = {0: "test", "one": 45, (1, 2.3): "Кортеж", True: 1, 35: [2, 3, 6, 7], "one": "один", False: "один"}
+# print(d)
+# d[(1, 2.3)] = 100
+# print(d)
+
+# d = {"one": 1, "two": 2}
+# print(d)
+# print(type(d))
+
+# d1 = dict(one='one', two=2)
+# print(d1)
+# print(type(d1))
+
+# d1 = dict([("one", 1), ("two", 2)])
+# d1 = dict(["on", ("two", 2)])
+# print(d1)
+
+# d = {x: x ** 2 for x in range(7)}
+# print(d)
+
+# d = {"one": 1, "two": 2, "three": 3}
+# print(d)
+# print("two" in d)
+# print(len(d))
+# for key in d:
+#     print(key, "->", d[key])
+# key = "six"
+# del d[key]
+# if key in d:
+#     print(d[key])
+# try:
+#     print(d[key])
+# except KeyError:
+#     print("Такого ключа не существует")
+# print(d)
+
+# d = {'x1': 3, 'x2': 7, 'x3': 5, 'x4': -1}
+# res = 1
+# for key in d:
+#     res *= d[key]
+# print(res)
+
+# d = dict()
+# d[1] = input("-> ")
+# d[2] = input("-> ")
+# d[3] = input("-> ")
+# d[4] = input("-> ")
+# d = {x: input("-> ") for x in range(1, 5)}
+# print(d)
+# try:
+#     dislike = int(input("Какой элемент исключить: "))
+#     del d[dislike]
+# except (ValueError, KeyError):
+#     print("Такого элемента не существует")
+# print(d)
