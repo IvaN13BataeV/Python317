2c4bc593af71499bb8b6f4ba4be51a5fdff49976
IvaN13BataeV <forward1301@mail.ru>
2024-03-20 19:53:12 +0500

    changed 18.03.24

--- main.py
+++ main.py
@@ -11,6 +11,8 @@
 # a = b
 # print(a, id(a))
 # print(b, id(b))
+import re
+
 
 # a = b = c = 3
 # print(a, b, c)
@@ -3536,3 +3538,236 @@
 # p5 = Point()
 #
 # print(Point.get_count())
+
+
+# class Change:
+#     @staticmethod
+#     def inc(x):
+#         return x + 1
+#
+#     @staticmethod
+#     def dec(x):
+#         return x - 1
+#
+#
+# ch = Change()
+# print(ch.inc(10), Change.dec(10))
+
+# class Fact:
+#     @staticmethod
+#     def max(*args):
+#         return max(args)
+#
+#     @staticmethod
+#     def min(*args):
+#         return min(args)
+#
+#     @staticmethod
+#     def fact(arg):
+#         factor = 1
+#         for i in range(1, arg + 1):
+#             factor *= i
+#         return factor
+#
+#     @staticmethod
+#     def avg(*args):
+#         return sum(args) / len(args)
+#
+#
+# print(Fact.max(3, 5, 7, 9))
+# print(Fact.min(3, 5, 7, 9))
+# print(Fact.fact(5))
+# print(Fact.avg(3, 5, 7, 9))
+
+
+# class Date:
+#     def __init__(self, day, month, year):
+#         self.day = day
+#         self.month = month
+#         self.year = year
+#
+#     @classmethod
+#     def from_string(cls, string_date):
+#         day, month, year = map(int, string_date.split("."))
+#         date1 = cls(day, month, year)
+#         return date1
+#
+#     @staticmethod
+#     def is_date_valid(date_as_string):
+#         if date_as_string.count('.') == 2:
+#             day, month, year = map(int, date_as_string.split("."))
+#             return day <= 31 and month <= 12 and year <= 3999
+#
+#     def string_to_db(self):
+#         return f"{self.year}-{self.month}-{self.day}"
+#
+#
+# dates = [
+#     "15.12.2024",
+#     "23-10-2023",
+#     "01.01.2021",
+#     "12.31.2020"
+# ]
+#
+# for d in dates:
+#     if Date.is_date_valid(d):
+#         date = Date.from_string(d)
+#         print(date.string_to_db())
+#     else:
+#         print("Неправильная дата или формат строки с датой")
+
+# date2 = Date.from_string("23.10.2023")
+# print(date2.string_to_db())
+# date3 = Date.from_string("15.12.2024")
+# print(date3.string_to_db())
+
+# day, month, year = map(int, string_date.split("."))
+# date = Date(day, month, year)
+# print(date.string_to_db())
+
+
+# class Account:
+#     rate_usd = 0.013
+#     rate_eur = 0.011
+#     suffix = "RUB"
+#     suffix_usd = "USD"
+#     suffix_eur = "EUR"
+#
+#     def __init__(self, surname, num, percent, value):
+#         self.surname = surname
+#         self.num = num
+#         self.percent = percent
+#         self.value = value
+#         print(f"Счет #{self.num} принадлежащий {self.surname} был открыт.")
+#         print("*" * 50)
+#
+#     def __del__(self):
+#         print("*" * 50)
+#         print(f"Счет #{self.num} принадлежащий {self.surname} был закрыт.")
+#
+#     @classmethod
+#     def set_usd_rate(cls, rate):
+#         cls.rate_usd = rate
+#
+#     @classmethod
+#     def set_eur_rate(cls, rate):
+#         cls.rate_eur = rate
+#
+#     @staticmethod
+#     def convert(value, rate):
+#         return value * rate
+#
+#     def convert_to_usd(self):
+#         usd_val = Account.convert(self.value, Account.rate_usd)
+#         print(f"Состояние счета: {usd_val} {Account.suffix_usd}")
+#
+#     def convert_to_eur(self):
+#         usd_val = Account.convert(self.value, Account.rate_eur)
+#         print(f"Состояние счета: {usd_val} {Account.suffix_eur}")
+#
+#     def edit_owner(self, surname):
+#         self.surname = surname
+#
+#     def add_percents(self):
+#         self.value += self.value * self.percent
+#         print("Проценты были успешно начислены!")
+#         self.print_balance()
+#
+#     def withdraw_money(self, val):
+#         if val > self.value:
+#             print(f"К сожалению у вас нет {val} {Account.suffix}")
+#         else:
+#             self.value -= val
+#             print(f"{val} {Account.suffix} было успешно снято!")
+#         self.print_balance()
+#
+#     def add_money(self, val):
+#         self.value += val
+#         print(f"{val} {Account.suffix} было успешно добавлено!")
+#         self.print_balance()
+#
+#     def print_balance(self):
+#         print(f"Текущий баланс {self.value} {Account.suffix}")
+#
+#     def print_info(self):
+#         print("Информация о счете:")
+#         print("-" * 20)
+#         print(f"#{self.num}")
+#         print(f"Владелец: {self.surname}")
+#         self.print_balance()
+#         print(f"Проценты: {self.percent:.0%}")
+#         print("-" * 20)
+#
+#
+# acc = Account("Долгих", "12345", 0.03, 1000)
+# acc.print_info()
+# acc.convert_to_usd()
+# acc.convert_to_eur()
+# print()
+# Account.set_usd_rate(2)
+# acc.convert_to_usd()
+# Account.set_eur_rate(3)
+# acc.convert_to_eur()
+# print()
+# acc.edit_owner("Дюма")
+# acc.print_info()
+# print()
+# acc.add_percents()
+# print()
+# acc.withdraw_money(3000)
+# print()
+# acc.withdraw_money(100)
+# print()
+# acc.add_money(5000)
+# print()
+# acc.withdraw_money(3000)
+# print()
+
+
+class UserDate:
+    def __init__(self, fio, old, ps, weight):
+        self.verify_fio(fio)
+        self.verify_old(old)
+        self.verify_weight(weight)
+        self.verify_ps(ps)
+
+        self.__fio = fio
+        self.__old = old
+        self.__password = ps
+        self.__weight = weight
+
+    @staticmethod
+    def verify_fio(fio):
+        if not isinstance(fio, str):
+            raise TypeError("ФИО должно быть строкой")
+        f = fio.split()
+        if len(f) != 3:
+            raise TypeError("Неверный формат ФИО")
+        letters = "".join(re.findall('[a-zа-яё-]', fio, re.IGNORECASE))
+        for s in f:
+            if len(s.strip(letters)) != 0:
+                raise TypeError("В ФИО можно использовать только буквы и дефис")
+
+    @staticmethod
+    def verify_old(old):
+        if not isinstance(old, int) or old < 14 or old > 120:
+            raise TypeError("Возраст должен быть числом от 14 до 120")
+
+    @staticmethod
+    def verify_weight(w):
+        if not isinstance(w, float) or w < 30:
+            raise TypeError("Вес должен быть вещественным числом от 30 и больше ")
+
+    @staticmethod
+    def verify_ps(ps):
+        if not isinstance(ps, str):
+            raise TypeError("Паспорт должен быть строкой")
+        s = ps.split()
+        if len(s) != 2 or len(s[0]) != 4 or len(s[1]) != 6:
+            raise TypeError("Неверный формат паспорта")
+        for p in s:
+            if not p.isdigit():
+                raise TypeError("Серия и номер паспорта должны быть числами")
+
+
+p1 = UserDate("Волков Игорь Николаевич", 24, "1234 567890", 80.8)
