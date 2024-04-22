f5f1b3a3dc8ea1ea644c5758511823325cb2c3c3
IvaN13BataeV <forward1301@mail.ru>
2024-04-22 05:20:14 +0500

    homework55

---
+++ hw_55.py
@@ -0,0 +1,12 @@
+import csv
+import json
+import requests
+
+# записываем данные из json формата в список
+todos = json.loads(requests.get("https://jsonplaceholder.typicode.com/todos").text)
+
+with open("todos.csv", 'w') as c:
+    writer = csv.DictWriter(c, delimiter=";", lineterminator="\r", fieldnames=todos[0].keys())
+    writer.writeheader()
+    for todo in todos:  # из текущего списка создаем csv файл с соответственным заполнением строк
+        writer.writerow(todo)
