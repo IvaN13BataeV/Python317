a3fbedb9349f54c1e10f952b8bfaec1ee163bcef
IvaN13BataeV <forward1301@mail.ru>
2024-05-19 01:09:02 +0500

    homework59

--- main.py
+++ main.py
@@ -6158,3 +6158,59 @@
 #     summa REAL,
 #     date BLOB)""")
 #     cur.execute("DROP TABLE users")
+
+# with sqlite3.connect("users.db") as con:
+#     cur = con.cursor()
+#
+#     cur.execute("""
+#     DROP TABLE person_table
+#     """)
+#
+#     cur.execute("""
+#     ALTER TABLE person_table
+#     DROP COLUMN home_address
+#     """)
+#
+#     cur.execute("""
+#     ALTER TABLE person_table
+#     RENAME COLUMN address TO home_address
+#     """)
+#
+#     cur.execute("""
+#     ALTER TABLE person_table
+#     ADD COLUMN address_add TEXT NOT NULL DEFAULT "Москва";
+#     """)
+#
+#     cur.execute("""
+#     ALTER TABLE person
+#     RENAME TO person_table;
+#     """)
+#
+#     cur.execute("""
+#     CREATE TABLE IF NOT EXISTS person(
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     name TEXT NOT NULL,
+#     phone BLOB NOT NULL DEFAULT "+79099000000",
+#     age INTEGER CHECK(age > 0 AND age < 100),
+#     email TEXT UNIQUE
+#     )""")
+# import sqlite3
+#
+# with sqlite3.connect("db_4.db") as con:
+#     cur = con.cursor()
+#
+#     cur.execute("""
+#     SELECT *
+#     FROM Ware
+#     ORDER BY Price DESC
+#     LIMIT 2, 5
+#     """)
+#
+#     # res = cur.fetchall()  # [(), ()]
+#     # # res = cur.fetchone()  # ()
+#     # # print(res)
+#     # # res = cur.fetchmany(10)  # [()]
+#     # print(res)
+#
+#     for res in cur:
+#         print(res)
